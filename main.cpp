07d5d8816b6711c115aed33db19529527ad969e8
erika <erikat.vin@gmail.com>
2023-07-27 20:11:05 +0000

    🌕

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <vector>
 
 // Abstract base class representing the concept of Samsara
 class Samsara {
@@ -54,12 +55,68 @@ public:
     }
 };
 
+// Derived class representing the concept of Dukkha Samudaya (the origin of suffering)
+class DukkhaSamudaya : public Samsara {
+public:
+    void transform() override {
+        std::cout << "Transforming craving into suffering...\n";
+    }
+};
+
+// Derived class representing the concept of Dukkha Nirodha (the cessation of suffering)
+class DukkhaNirodha : public Samsara {
+public:
+    void transform() override {
+        std::cout << "Transforming cessation into peace...\n";
+    }
+};
+
+// Derived class representing the concept of Dukkha Nirodha Gamini Patipada (the path leading to the cessation of suffering)
+class DukkhaNirodhaGaminiPatipada : public Samsara {
+public:
+    void transform() override {
+        std::cout << "Transforming practice into liberation...\n";
+    }
+};
+
+// Derived class representing the concept of Nirvana (enlightenment)
+class Nirvana : public Samsara {
+public:
+    void transform() override {
+        std::cout << "Transforming ignorance into enlightenment...\n";
+    }
+};
+
 int main() {
     auto anicca = std::make_unique<Anicca>(); // Create a unique pointer to an Anicca object
     Sunyata sunyataAnicca(std::move(anicca)); // Create a Sunyata object with the Anicca object
     Vipassana meditationAnicca(sunyataAnicca); // Create a Vipassana object with the Sunyata object
 
     meditationAnicca.observe(); // Call the observe function of the Vipassana object
+
+    auto dukkhaSamudaya = std::make_unique<DukkhaSamudaya>(); // Create a unique pointer to a DukkhaSamudaya object
+    Sunyata sunyataDukkhaSamudaya(std::move(dukkhaSamudaya)); // Create a Sunyata object with the DukkhaSamudaya object
+    Vipassana meditationDukkhaSamudaya(sunyataDukkhaSamudaya); // Create a Vipassana object with the Sunyata object
+
+    meditationDukkhaSamudaya.observe(); // Call the observe function of the Vipassana object
+
+    auto dukkhaNirodha = std::make_unique<DukkhaNirodha>(); // Create a unique pointer to a DukkhaNirodha object
+    Sunyata sunyataDukkhaNirodha(std::move(dukkhaNirodha)); // Create a Sunyata object with the DukkhaNirodha object
+    Vipassana meditationDukkhaNirodha(sunyataDukkhaNirodha); // Create a Vipassana object with the Sunyata object
+
+    meditationDukkhaNirodha.observe(); // Call the observe function of the Vipassana object
+
+    auto dukkhaNirodhaGaminiPatipada = std::make_unique<DukkhaNirodhaGaminiPatipada>(); // Create a unique pointer to a DukkhaNirodhaGaminiPatipada object
+    Sunyata sunyataDukkhaNirodhaGaminiPatipada(std::move(dukkhaNirodhaGaminiPatipada)); // Create a Sunyata object with the DukkhaNirodhaGaminiPatipada object
+    Vipassana meditationDukkhaNirodhaGaminiPatipada(sunyataDukkhaNirodhaGaminiPatipada); // Create a Vipassana object with the Sunyata object
+
+    meditationDukkhaNirodhaGaminiPatipada.observe(); // Call the observe function of the Vipassana object
+
+    auto nirvana = std::make_unique<Nirvana>(); // Create a unique pointer to a Nirvana object
+    Sunyata sunyataNirvana(std::move(nirvana)); // Create a Sunyata object with the Nirvana object
+    Vipassana meditationNirvana(sunyataNirvana); // Create a Vipassana object with the Sunyata object
+
+    meditationNirvana.observe(); // Call the observe function of the Vipassana object
 
     return 0;
 }
